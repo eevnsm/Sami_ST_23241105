c70cbd09717bff7d0c59fbb23d424ad82e3c5914
eevnsm <147676383+eevnsm@users.noreply.github.com>
2024-05-16 14:12:22 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+a = 3
+b = 6
+#lebih dari
+
+hasil = a > b
+print (a, '>', b, '=', hasil)
+
+#kurang dari
+hasil = a < b
+print (a, '>', b, '=', hasil)
+
+# lebih dari sama dengan
+hasil = a >= b
+print (a, '>=', b, '=', hasil)
+
+#kurang dari sama dengan
+hasil = a <= b
+print (a, '<=', b, '=', hasil)
+
+#is
+
+hasil = a is 2
+print (a, 'is', 2, '=', hasil)
+
+#is not
+hasil = a is not 7
+print (a, 'is not', 7, '=', hasil)
